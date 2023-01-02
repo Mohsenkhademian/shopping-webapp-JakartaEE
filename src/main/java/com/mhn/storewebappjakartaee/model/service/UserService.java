63e1378a1b4b3e92e72453afdc7f978c5936ed6f
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 12:49:05 +0330

    added service layer

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/service/UserService.java
@@ -0,0 +1,54 @@
+package com.mhn.storewebappjakartaee.model.service;
+
+import com.mhn.storewebappjakartaee.model.entity.User;
+import com.mhn.storewebappjakartaee.model.repository.CRUDRepository;
+import com.mhn.storewebappjakartaee.model.service.base.ServiceImpl;
+
+import java.util.List;
+
+
+public class UserService extends ServiceImpl<User, Long> {
+    private static final UserService userService = new UserService();
+
+    private UserService() {
+    }
+
+    public static UserService getUserService() {
+        return userService;
+    }
+
+    @Override
+    public User save(User user) throws Exception {
+        try (CRUDRepository<User,Long> crudRepository = new CRUDRepository<>()) {
+            return crudRepository.insert(user);
+        }
+    }
+
+    @Override
+    public User update(User user) throws Exception {
+        try (CRUDRepository<User,Long> crudRepository = new CRUDRepository<>()) {
+            return crudRepository.update(user);
+        }
+    }
+
+    @Override
+    public User delete(Long id) throws Exception {
+        try (CRUDRepository<User,Long> crudRepository = new CRUDRepository<>()) {
+            return crudRepository.delete(User.class , id);
+        }
+    }
+
+    @Override
+    public User findById(Long id) throws Exception {
+        try (CRUDRepository<User,Long> crudRepository = new CRUDRepository<>()) {
+            return crudRepository.selectById(User.class , id);
+        }
+    }
+
+    @Override
+    public List<User> findAll() throws Exception {
+        try (CRUDRepository<User,Long> crudRepository = new CRUDRepository<>()) {
+            return crudRepository.selectAll(User.class);
+        }
+    }
+}
---
+++ src/main/java/com/mhn/storewebappjakartaee/model/service/base/ServiceImpl.java
@@ -0,0 +1,18 @@
+package com.mhn.storewebappjakartaee.model.service.base;
+
+
+import java.util.List;
+
+
+public abstract class ServiceImpl <T, ID>{
+    public abstract T save(T t) throws Exception;
+
+    public abstract T delete(ID id) throws Exception;
+
+    public abstract T update(T t) throws Exception;
+
+    public abstract List<T> findAll() throws Exception;
+
+    public abstract T findById(ID id) throws Exception;
+
+}
