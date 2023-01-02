bf1c678cc2b7c8e428d3c2cc59b89cddd9a7a1da
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 12:33:22 +0330

    added Storage Entity

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/entity/Storage.java
@@ -0,0 +1,30 @@
+package com.mhn.storewebappjakartaee.model.entity;
+
+import com.mhn.storewebappjakartaee.model.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity(name = "storageEntity")
+@Table(name = "storage")
+public class Storage extends BaseEntity {
+    @Column(name = "c_name")
+    private String name;
+    @Column(name = "c_address")
+    private String address;
+    @Column(name = "c_capacity")
+    private int capacity;
+    @Column(name = "c_availablecapacity")
+    private int availableCapacity;
+
+    /*@OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "storage_id")
+    private List<Item> items;*/
+
+}
