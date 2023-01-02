194b2d94eabb2dc99440d537e38844986e5e0c57
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 13:00:46 +0330

    added persistance file and test in Main class

---
+++ src/main/java/com/mhn/storewebappjakartaee/Main.java
@@ -0,0 +1,11 @@
+package com.mhn.storewebappjakartaee;
+
+import com.mhn.storewebappjakartaee.model.entity.User;
+import com.mhn.storewebappjakartaee.model.service.UserService;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        User user = User.builder().userName("moshen").password("moshen123").build();
+        UserService.getUserService().save(user);
+    }
+}
