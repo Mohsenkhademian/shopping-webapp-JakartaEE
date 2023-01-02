eb307d0c4591c434a18be1020acdd112197a8a11
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 23:28:46 +0330

    ye paiam baraie messbahh

--- src/main/java/com/mhn/storewebappjakartaee/Main.java
+++ src/main/java/com/mhn/storewebappjakartaee/Main.java
@@ -7,5 +7,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         User user = User.builder().userName("moshen").password("moshen123").build();
         UserService.getUserService().save(user);
+
+        //;sdkflsdkfjlasdkfjlasdkfjlsadkjf;dksf;lskdf;l
+        //lsdfjlskdafjlksajdflskdjfl
+
+        //dslkfladkflaskdflskdfllsdf
+
+        //sdlfjsladfkjlsdkaflsakdfls
+
     }
 }
