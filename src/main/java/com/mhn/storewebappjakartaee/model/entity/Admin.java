9b4e5e075dbb3f9fc93c859aa4918622b8168e15
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 11:55:32 +0330

    added Admin Entity

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/entity/Admin.java
@@ -0,0 +1,22 @@
+package com.mhn.storewebappjakartaee.model.entity;
+
+import com.mhn.storewebappjakartaee.model.entity.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity(name = "userAdmin")
+@Table(name = "admin")
+public class Admin extends BaseEntity {
+    @Column(name = "c_username")
+    private String userName;
+    @Column(name = "c_password")
+    private String password;
+
+}
