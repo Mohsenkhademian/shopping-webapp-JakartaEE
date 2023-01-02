c104762e4c2afdd373c247a73c8df979c9e4cd36
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 12:28:16 +0330

    added comment for later work in class

--- src/main/java/com/mhn/storewebappjakartaee/model/entity/Item.java
+++ src/main/java/com/mhn/storewebappjakartaee/model/entity/Item.java
@@ -3,8 +3,11 @@ package com.mhn.storewebappjakartaee.model.entity;
 import com.mhn.storewebappjakartaee.model.entity.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,4 +23,7 @@ public class Item extends BaseEntity {
     private String description;
     @Column(name = "c_price")
     private double price;
+
+    /*@OneToMany(mappedBy = "product")
+    private List<Offer> offers;*/
 }
