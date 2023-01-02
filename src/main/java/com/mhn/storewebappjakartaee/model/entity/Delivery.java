c54fd8849d5f17dcea43428fa3575a9a76b96e1e
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-02 12:15:44 +0330

    added Delivery Entity

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/entity/Delivery.java
@@ -0,0 +1,31 @@
+package com.mhn.storewebappjakartaee.model.entity;
+
+import com.mhn.storewebappjakartaee.model.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity(name = "deliveryEntity")
+@Table(name = "delivery")
+public class Delivery extends BaseEntity {
+    private String recipientName;
+    private String recipientAddress;
+
+
+    private Boolean deliveryStatus;
+
+    @Temporal(TemporalType.DATE)
+    private Date deliveryDate;
+
+    @Temporal(TemporalType.TIME)
+    private Date deliveryTime;
+
+   /* @ElementCollection
+    private List<String> items;*/
+}
