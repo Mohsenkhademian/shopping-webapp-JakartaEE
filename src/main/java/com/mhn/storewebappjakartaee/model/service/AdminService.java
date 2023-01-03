be21f41e562db50d484d54d663fd89d162628322
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-03 18:28:22 +0330

    added AdminService Class

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/service/AdminService.java
@@ -0,0 +1,52 @@
+package com.mhn.storewebappjakartaee.model.service;
+
+import com.mhn.storewebappjakartaee.model.entity.Admin;
+import com.mhn.storewebappjakartaee.model.entity.User;
+import com.mhn.storewebappjakartaee.model.repository.CRUDRepository;
+import com.mhn.storewebappjakartaee.model.service.base.ServiceImpl;
+
+import java.util.List;
+
+public class AdminService extends ServiceImpl<Admin , Long> {
+    private static final AdminService adminService = new AdminService();
+
+    private AdminService(){
+    }
+
+    public static AdminService getAdminService(){return adminService; }
+
+    @Override
+    public Admin save(Admin admin) throws Exception {
+        try (CRUDRepository<Admin , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.insert(admin);
+        }
+    }
+
+    @Override
+    public Admin delete(Long id) throws Exception {
+        try (CRUDRepository<Admin , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.delete(Admin.class , id);
+        }
+    }
+
+    @Override
+    public Admin update(Admin admin) throws Exception {
+        try (CRUDRepository<Admin , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.update(admin);
+        }
+    }
+
+    @Override
+    public List<Admin> findAll() throws Exception {
+        try (CRUDRepository<Admin , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.selectAll(Admin.class);
+        }
+    }
+
+    @Override
+    public Admin findById(Long id) throws Exception {
+        try (CRUDRepository<Admin , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.selectById(Admin.class , id);
+        }
+    }
+}
