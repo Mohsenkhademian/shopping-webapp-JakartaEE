5f61fe18340d8b7334a197c166445528c1e6a080
Mohsen Khademian <90333755+Mohsenkhademian@users.noreply.github.com>
2023-01-03 17:07:38 +0330

    Update Main.java

--- src/main/java/com/mhn/storewebappjakartaee/Main.java
+++ src/main/java/com/mhn/storewebappjakartaee/Main.java
@@ -7,7 +7,5 @@ public class Main {
     public static void main(String[] args) throws Exception {
         User user = User.builder().userName("moshen").password("moshen123").build();
         UserService.getUserService().save(user);
-        //////sfnjnfsf
-
     }
 }
