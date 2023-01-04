bbd2e410d2dd9591fd842ac920cef21a0529d59f
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-04 11:06:13 +0330

    added CustomerService

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/service/CustomerService.java
@@ -0,0 +1,51 @@
+package com.mhn.storewebappjakartaee.model.service;
+
+import com.mhn.storewebappjakartaee.model.entity.Customer;
+import com.mhn.storewebappjakartaee.model.repository.CRUDRepository;
+import com.mhn.storewebappjakartaee.model.service.base.ServiceImpl;
+
+import java.util.List;
+
+public class CustomerService extends ServiceImpl<Customer, Long> {
+    private static CustomerService customerService = new CustomerService();
+
+    private CustomerService(){
+    }
+
+    public static CustomerService getCustomerService(){return customerService;}
+
+    @Override
+    public Customer save(Customer customer) throws Exception {
+        try (CRUDRepository<Customer, Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.insert(customer);
+        }
+    }
+
+    @Override
+    public Customer delete(Long id) throws Exception {
+        try (CRUDRepository<Customer, Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.delete(Customer.class,id);
+        }
+    }
+
+    @Override
+    public Customer update(Customer customer) throws Exception {
+        try (CRUDRepository<Customer, Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.update(customer);
+        }
+    }
+
+    @Override
+    public List<Customer> findAll() throws Exception {
+        try (CRUDRepository<Customer , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.selectAll(Customer.class);
+        }
+    }
+
+    @Override
+    public Customer findById(Long id) throws Exception {
+        try (CRUDRepository<Customer , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.selectById(Customer.class , id);
+        }
+    }
+}
