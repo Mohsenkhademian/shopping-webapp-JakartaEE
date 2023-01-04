34b3cb93f7a13deaadb850b2730a833ac25afe51
MohsenKhademian <mohsenkhademian18@gmail.com>
2023-01-04 11:28:15 +0330

    added DeliveryService

---
+++ src/main/java/com/mhn/storewebappjakartaee/model/service/DeliveryService.java
@@ -0,0 +1,53 @@
+package com.mhn.storewebappjakartaee.model.service;
+
+import com.mhn.storewebappjakartaee.model.entity.Customer;
+import com.mhn.storewebappjakartaee.model.entity.Delivery;
+import com.mhn.storewebappjakartaee.model.repository.CRUDRepository;
+import com.mhn.storewebappjakartaee.model.service.base.ServiceImpl;
+
+import java.util.List;
+
+public class DeliveryService extends ServiceImpl<Delivery , Long> {
+    private static DeliveryService deliveryService = new DeliveryService();
+
+    private DeliveryService(){
+    }
+
+    public static DeliveryService getDeliveryService(){return getDeliveryService();}
+
+
+    @Override
+    public Delivery save(Delivery delivery) throws Exception {
+        try (CRUDRepository<Delivery, Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.insert(delivery);
+        }
+    }
+
+    @Override
+    public Delivery delete(Long id) throws Exception {
+        try (CRUDRepository<Delivery, Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.delete(Delivery.class,id);
+        }
+    }
+
+    @Override
+    public Delivery update(Delivery delivery) throws Exception {
+        try (CRUDRepository<Delivery, Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.update(delivery);
+        }
+    }
+
+    @Override
+    public List<Delivery> findAll() throws Exception {
+        try (CRUDRepository<Delivery , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.selectAll(Delivery.class);
+        }
+    }
+
+    @Override
+    public Delivery findById(Long id) throws Exception {
+        try (CRUDRepository<Delivery , Long> crudRepository = new CRUDRepository<>()){
+            return crudRepository.selectById(Delivery.class , id);
+        }
+    }
+}
